7933af44fba703f5b11fb0ad2967539c3ae4a993
Pedro Gatti <pedro.gatti@escuelacangallo.edu.ar>
2024-11-23 01:29:09 -0300

    cambie el diseño y se despliega el menu

--- main.js
+++ main.js
@@ -1,26 +1,43 @@
 $(document).ready(function () {
     let productos = false;
-    let codigo = "";
-    let a = "";
-
+    $("#menuDesp").hide();
+    let desplegado = false;
+    var menu = document.getElementById("menu");
+    menu.addEventListener("click", function(){
+        if (desplegado){
+            $("#menuDesp").hide();
+            desplegado = false;
+        } else{
+            $("#menuDesp").show();
+            desplegado = true;
+        }
+    })
     $("#boton").on("click", function () {
         if (productos) {
             $("#productos").hide();
             $("#cuerpo").show();
             productos = false;
         } else {
+            let codigo = "";
+            let a = "";
             let f = 1;
             $("#cuerpo").hide();
+            $("#productos").show();
             productos = true;
             for (let i = 0; i < 2; i++) {
                 for (let e = 0; e < 2; e++) {
-                    a = a + `<div class = 'producto'>Producto ${f}</div>`;
+                    a = a + `<div id = 'producto${f}' class = 'producto col-6'>Producto ${f}</div>`;
                     f++;
                 }
-                codigo = codigo + `<div class = 'a col-4'>${a}</div>`;
+                codigo = codigo + `<div id = 'conj-prod${i+1}' class = 'a col-4'>${a}</div>`;
                 a = "";
                 $("#productos").html(codigo);
             }
+            for (let i = 0; i < 2; i++){
+                document.getElementById(`conj-prod${i+1}`).classList.add("fondoVerde", "letraAmarilla");
+            }
+            document.getElementById("productos").classList.add("productos");
+            document.getElementsById("footer").classList.add("footerAbajo");
         }
 })
 })
